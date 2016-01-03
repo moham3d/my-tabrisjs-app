fd0ef7c269b6d312ad8d12b7b3926f9c499c7411
moham3d <moham3d@live.com>
2016-01-03 13:34:41 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,84 +1,162 @@
-var MARGIN = 12;
-var MARGIN_LARGE = 24;
-var LARGE_FONT = "20px";
-
-var cpsCount = 0;
-var startTime = new Date().getTime();
-var taskId;
+var message;
 
 var page = tabris.create("Page", {
-  title: "Setting timers and intervals",
+  title: "Oceanic Flight 815 Booking",
   topLevel: true
 });
 
-var statusTextView = tabris.create("TextView", {
-  text: "Last update: <none>",
-  layoutData: {left: MARGIN, top: MARGIN_LARGE, right: MARGIN},
-  font: LARGE_FONT
+tabris.create("TextView", {
+  id: "firstNameLabel",
+  alignment: "left",
+  text: "First Name:"
 }).appendTo(page);
 
-var cpsTextView = tabris.create("TextView", {
-  text: "Calls per second: <none>",
-  layoutData: {left: MARGIN, top: [statusTextView, MARGIN], right: MARGIN},
-  font: LARGE_FONT
+tabris.create("TextInput", {
+  id: "firstNameInput",
+  message: "First Name"
 }).appendTo(page);
 
-var delayTextView = tabris.create("TextView", {
-  text: "Delay (ms)",
-  layoutData: {left: MARGIN, top: [cpsTextView, MARGIN_LARGE]}
+tabris.create("TextView", {
+  id: "lastNameLabel",
+  text: "Last Name:"
 }).appendTo(page);
 
-var delayTextInput = tabris.create("TextInput", {
-  text: "1000",
-  message: "Delay (ms)",
-  layoutData: {left: [delayTextView, MARGIN], top: [cpsTextView, MARGIN_LARGE]}
+tabris.create("TextInput", {
+  id: "lastNameInput",
+  message: "Last Name"
 }).appendTo(page);
 
-var repeatCheckbox = tabris.create("CheckBox", {
-  text: "Repeat",
-  layoutData: {left: MARGIN, top: [delayTextInput, MARGIN]}
+tabris.create("TextView", {
+  id: "passphraseLabel",
+  text: "Passphrase:"
 }).appendTo(page);
 
-var startButton = tabris.create("Button", {
-  text: "Start timer",
-  layoutData: {left: ["50%", MARGIN / 4], top: [repeatCheckbox, MARGIN_LARGE], right: MARGIN}
+tabris.create("TextInput", {
+  id: "passphraseInput",
+  type: "password",
+  message: "Passphrase"
+}).appendTo(page);
+
+tabris.create("TextView", {
+  id: "countryLabel",
+  text: "Country:"
+}).appendTo(page);
+
+tabris.create("Picker", {
+  id: "countryPicker",
+  items: ["Germany", "Canada", "USA", "Bulgaria"]
+}).appendTo(page);
+
+tabris.create("TextView", {
+  id: "classLabel",
+  text: "Class:"
+}).appendTo(page);
+
+tabris.create("Picker", {
+  id: "classPicker",
+  items: ["Business", "Economy", "Economy Plus"]
+}).appendTo(page);
+
+tabris.create("TextView", {
+  id: "seatLabel",
+  text: "Seat:"
+}).appendTo(page);
+
+tabris.create("RadioButton", {
+  id: "window",
+  text: "Window"
+}).appendTo(page);
+
+tabris.create("RadioButton", {
+  id: "aisle",
+  text: "Aisle"
+}).appendTo(page);
+
+tabris.create("RadioButton", {
+  id: "dontCareButton",
+  text: "Don't care",
+  selection: true
+}).appendTo(page);
+
+tabris.create("Composite", {
+  id: "luggagePanel"
+}).append(
+  tabris.create("TextView", {
+    id: "luggageLabel",
+    text: "Luggage:"
+  })
+).append(
+  tabris.create("TextView", {
+    id: "luggageWeight",
+    text: "0 Kg"
+  })
+).append(
+  tabris.create("Slider", {
+    id: "luggageSlider"
+  }).on("change:selection", function(widget, selection) {
+    page.find("#luggageWeight").set("text", selection + " Kg");
+  })
+).appendTo(page);
+
+tabris.create("CheckBox", {
+  id: "veggie",
+  text: "Vegetarian"
+}).appendTo(page);
+
+tabris.create("Button", {
+  id: "done",
+  text: "Place Reservation",
+  background: "#8b0000",
+  textColor: "white"
 }).on("select", function() {
-  var delay = parseInt(delayTextInput.get("text"));
-  if (repeatCheckbox.get("selection")) {
-    taskId = setInterval(updateStatusTextViews, delay);
-  } else {
-    taskId = setTimeout(function() {
-      updateStatusTextViews();
-      enableTimerStart(true);
-    }, delay);
+  populateMessage();
+}).appendTo(page);
+
+page.apply({
+  "#firstNameLabel": {layoutData: {left: 10, top: 18, width: 120}},
+  "#firstNameInput": {layoutData: {left: "#firstNameLabel 10", right: 10, baseline: "#firstNameLabel"}},
+  "#lastNameLabel": {layoutData: {left: 10, top: "#firstNameLabel 18", width: 120}},
+  "#lastNameInput": {layoutData: {left: "#lastNameLabel 10", right: 10, baseline: "#lastNameLabel"}},
+  "#passphraseLabel": {layoutData: {left: 10, top: "#lastNameLabel 18", width: 120}},
+  "#passphraseInput": {layoutData: {left: "#passphraseLabel 10", right: 10, baseline: "#passphraseLabel"}},
+  "#countryLabel": {layoutData: {left: 10, top: "#passphraseLabel 18", width: 120}},
+  "#countryPicker": {layoutData: {left: "#countryLabel 10", right: 10, baseline: "#countryLabel"}},
+  "#seatLabel": {layoutData: {left: 10, top: "#classLabel 18", width: 120}},
+  "#window": {layoutData: {left: "#seatLabel 10", right: 10, baseline: "#seatLabel"}},
+  "#aisle": {layoutData: {left: "#seatLabel 10", right: 10, top: "#seatLabel 10"}},
+  "#classLabel": {layoutData: {left: 10, top: "#countryLabel 18", width: 120}},
+  "#classPicker": {layoutData: {left: "#classLabel 10", right: 10, baseline: "#classLabel"}},
+  "#dontCareButton": {layoutData: {left: "#seatLabel 10", right: 10, top: "#aisle 10"}},
+  "#luggagePanel": {layoutData: {left: 10, top: "#dontCareButton 18", right: 10}},
+  "#luggageLabel": {layoutData: {left: 0, centerY: 0, width: 120}},
+  "#luggageWeight": {layoutData: {right: 10, centerY: 0, width: 50}},
+  "#luggageSlider": {layoutData: {left: "#luggageLabel 10", right: "#luggageWeight 10", centerY: 0}},
+  "#veggie": {layoutData: {left: "#seatLabel 10", right: 10, top: "#luggagePanel 10"}},
+  "#done": {layoutData: {left: 10, right: 10, top: "#veggie 18"}}
+});
+
+function populateMessage() {
+  if (message) {
+    message.dispose();
   }
-  enableTimerStart(false);
-}).appendTo(page);
+  message = tabris.create("TextView", {
+    layoutData: {left: 10, right: 10, top: "#done 10"},
+    text: "Flight booked for: " + createName() + "\nSeating: " + createSeating()
+  }).appendTo(page);
+}
 
-var cancelButton = tabris.create("Button", {
-  text: "Cancel timer",
-  selection: false,
-  layoutData: {left: MARGIN, top: [repeatCheckbox, MARGIN_LARGE], right: ["50%", MARGIN / 4]}
-}).on("select", function() {
-  clearTimeout(taskId);
-  enableTimerStart(true);
-}).appendTo(page);
+function createName() {
+  return [page.children("#firstNameInput").get("text"), page.children("#lastNameInput").get("text")].join(" ");
+}
+
+function createSeating() {
+  var seating = "Anywhere";
+  page.children("RadioButton").forEach(function(button) {
+    if (button.get("selection")) {
+      seating = button.get("text");
+    }
+  });
+  return seating;
+}
 
 page.open();
-
-function updateStatusTextViews() {
-  cpsCount++;
-  var curTime = new Date().getTime();
-  var diff = curTime - startTime;
-  if (diff >= 1000) {
-    cpsTextView.set("text", "Calls per second: " + cpsCount);
-    cpsCount = 0;
-    startTime = curTime;
-  }
-  statusTextView.set("text", "Last update: " + new Date().getTime().toString());
-}
-
-function enableTimerStart(available) {
-  startButton.set("enabled", available);
-  cancelButton.set("enabled", !available);
-}
